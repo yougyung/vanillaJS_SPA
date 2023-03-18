b5fd4afcaa0941ed259e94c59e70802287e83357
Youkyeong <75975946+youkyeong60@users.noreply.github.com>
2023-03-19 01:49:10 +0900

    Fix: 동적 라우팅

--- src/core/router.js
+++ src/core/router.js
@@ -1,13 +1,18 @@
 import { routeList } from '../pages/routes.js';
 
-export const router = async () => {
-	const { pathname } = window.location;
-	const path = routeList.find((route) => pathname === route.path);
-	path ? console.log(path) : (path = routeList[0]);
+export const router = () => {
+	let { pathname } = window.location;
+	let path = routeList.find((route) => {
+		return pathname.match(pathToRegex(route.path));
+	});
+	console.log(path);
+	path ? null : (path = routeList[0]);
 
 	const view = new path.view();
-	document.querySelector('#app').innerHTML = await view.template();
+	document.querySelector('#app').innerHTML = view.template();
 };
+
+const pathToRegex = (path) => new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
 
 export const navigate = (state, title, url) => {
 	window.history.pushState(state, title, url);
